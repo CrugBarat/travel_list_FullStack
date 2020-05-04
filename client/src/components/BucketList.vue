3c383e3adec6dec085541a120d3165a2aa2421dd
Craig Barratt <craigtbarratt@gmail.com>
2020-05-04 23:01:28 +0100

    adds v-bind class

--- client/src/components/BucketList.vue
+++ client/src/components/BucketList.vue
@@ -2,7 +2,7 @@
   <div id="favourite_countries">
     <h2>Bucket List</h2>
     <ul>
-      <li v-for="country in bucketList">{{country.name}} <img class="small-flag" :src="country.flag"/> <button v-if="!country.visited" v-on:click="updateList(country)">Visit!</button></li>
+      <li  v-for="country in bucketList" v-bind:class="visitedClass(country )">{{country.name}} <img class="small-flag" :src="country.flag"/> <button v-if="!country.visited" v-on:click="updateList(country)">Visit!</button></li>
     </ul>
   </div>
 </template>
@@ -12,17 +12,26 @@ import BucketService from '@/services/BucketService.js';
 import {eventBus} from '@/main.js';
 
 export default {
-  name: 'bucket-list',
-  props: ['bucketList'],
-  methods: {
-    updateList(country){
-      const updatedVisit = {visited: true}
-      BucketService.updateCountry(country._id, updatedVisit)
-      .then((country) => eventBus.$emit('country-updated', country))
+    name: 'bucket-list',
+    props: ['bucketList'],
+    methods: {
+      updateList(country){
+        const updatedVisit = {visited: true}
+        BucketService.updateCountry(country._id, updatedVisit)
+        .then((country) => eventBus.$emit('country-updated', country))
+      },
+      visitedClass(country) {
+        return country.visited ? "visited" : "not-visited"
+      }
     }
   }
-}
 </script>
 
 <style lang="css" scoped>
+  .visited {
+    color: green;
+  }
+  .not-visited {
+    color: red;
+  }
 </style>
